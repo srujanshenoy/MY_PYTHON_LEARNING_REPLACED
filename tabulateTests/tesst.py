bb345d6f38edb2c233be8f245fbb403b30aefadd
srujanshenoy <srujanshenoy@gmail.com>
2023-05-28 13:44:27 +0000

    commit

--- tabulateTests/tesst.py
+++ tabulateTests/tesst.py
@@ -1,13 +1,14 @@
 from tabulate import tabulate
 
 def print_table(data):
-    print(tabulate(data, headers='keys', tablefmt='fancy_grid', showindex='always'))  
+    print(tabulate(data, tablefmt='fancy_grid', showindex='always'))  
 
 def print_table_with_headers(data):
     print(tabulate(data, headers='keys', tablefmt='fancy_grid', showindex='always'))
 
 if __name__ == "__main__":
-    print_table_with_headers([
+    # print_table_with_headers([
+    print_table([
         ['Name', 'Age', 'Gender'],
         ['John', 20, 'Male'],
         ['Mary', 25, 'Female'],
