30d88a858def2be6039f379d6c4c48caefb015f1
Shwetha S <srujanshenoy@gmail.com>
2023-12-29 16:45:56 +0530

    Added Somthing

--- test file.py
+++ test file.py
@@ -1,3 +1,15 @@
-from time import strftime 
+import winsound
+import time as t
 
-print(int(strftime("%S")))
+fixed_time = 10
+duration = fixed_time
+
+while duration > 0:
+    if duration == fixed_time: print(f"STARTED timer for {fixed_time} SECONDS")
+    if duration == fixed_time/2: print(f"HALFWAY THERE {fixed_time / 2}")
+    if duration == 1: print("DONE")  
+
+    t.sleep(1)
+    duration -= 1
+
+winsound.MessageBeep()
